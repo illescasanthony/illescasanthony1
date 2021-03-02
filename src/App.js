4a2b834b271b6489b5a8df30b487fb4bca4fff60
Anthony Illescas <anthonyillescas@AnthonysMacBook.home>
2021-03-02 16:51:03 -0500

    modified App.js to say {Anthony Illescas}

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          My name is Anthony Illescas and I'm going to master React! 
         </p>
         <a
           className="App-link"
